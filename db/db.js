6006538bd406e03d7cae11f582837064c3b1ff59
Unknown <cissou98@gmail.com>
2017-02-28 11:26:19 +0100

    Document +db init function done

--- db/db.js
+++ db/db.js
@@ -1,44 +1,60 @@
-var knex = require("knex")({});
+var knex = require('knex')({client: 'sqlite3'});
 var sqlite = require("sqlite3");
 var handle;
-var init = function() {
-    handle = new sqlite.Database("Data.db");
-    handle.serialize(function() {
-        handle.run(knex.schema.createTableIfNotExists("users", function(table) { //Users table
-          table.increment("id").primary();
-          table.string("username");
-          table.string("password");
-          table.integer("type");
-          table.string("fname");
-          table.string("lname");
-          table.string("dob");
-          table.string("email");
-        }).toString());
+RANK = {
+  STUDENT: 0,
+  PROF: 1,
+  ADMIN: 2
+};
+module.exports = {
+  handle: handle,
+  RANK: RANK,
+  init: function() {
+      handle = new sqlite.Database("Data.db");
+      handle.serialize(function() {
+          handle.run(knex.schema.createTableIfNotExists("users", function(table) { //Users table
+            table.increments("id").primary();
+            table.string("username");
+            table.string("password");
+            table.integer("rank");
+            table.string("fname");
+            table.string("lname");
+            table.string("dob");
+            table.string("email");
+          }).toString());
 
-        handle.run(knex.schema.createTableIfNotExists("student", function(table) {//Student table
-          table.increment("id").primary();
-          table.integer("userid");
-          table.integer("profid");
-          table.integer("timeDiff");
-          table.integer("status");
-          table.boolean("isBlocked");
-        }).toString());
+          handle.run(knex.schema.createTableIfNotExists("student", function(table) {//Student table
+            table.increments("id").primary();
+            table.integer("userid");
+            table.integer("profid");
+            table.integer("timeDiff");
+            table.integer("status");
+            table.boolean("isBlocked");
+          }).toString());
 
 
-        handle.run(knex.schema.createTableIfNotExists("timeoff", function(table) {//timeoff table
-          table.increment("id").primary();
-          table.string("date1");
-          table.string("date2");
-          table.string("desc");
-        }).toString());
+          handle.run(knex.schema.createTableIfNotExists("timeoff", function(table) {//timeoff table
+            table.increments("id").primary();
+            table.string("date1");
+            table.string("date2");
+            table.string("desc");
+          }).toString());
 
-        handle.run(knex.schema.createTableIfNotExists("timeoff", function(table) {//logs table
-          table.increment("id").primary();
-          table.string("tag");
-          table.integer("studentid");
-          table.integer("nstatus");
-          table.string("date");
-          table.string("location");
-        }).toString());
-    });
+          handle.run(knex.schema.createTableIfNotExists("timeoff", function(table) {//logs table
+            table.increments("id").primary();
+            table.string("tag");
+            table.integer("studentid");
+            table.integer("nstatus");
+            table.string("date");
+            table.string("location");
+          }).toString());
+      });
+      handle.get("SELECT * FROM users WHERE rank=2",(err,data) => {
+        if(data === undefined)
+        {
+          handle.run(knex("users").insert({username: "admin",password: "password",rank: RANK.ADMIN, fname: "Administrator", lname: "", dob: "", email: ""}).toString());
+        }
+      });
+
+  }
 };
