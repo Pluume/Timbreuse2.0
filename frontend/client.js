7d168c176cba50f3640962e024f7e8b95d47cb92
Unknown <cissou98@gmail.com>
2017-03-02 16:08:43 +0100

    House keeping

--- frontend/client.js
+++ frontend/client.js
@@ -5,35 +5,35 @@ const path = require('path');
 const url = require('url');
 function createWindow()
 {
-  global.mwin = new BrowserWindow({
+    global.mwin = new BrowserWindow({
       width: 800,
-      height: 600
-  });
-  global.mwin.loadURL(url.format({
-      pathname: path.join(__dirname, 'client.html'),
-      protocol: 'file:',
-      slashes: true
-  }));
-  global.mwin.webContents.openDevTools();
-  global.mwin.on('closed', function () {
-     mainWindow = null;
-   });
+        height: 600
+
+    });
+    global.mwin.loadURL(url.format({
+        pathname: path.join(__dirname, 'client.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    global.mwin.webContents.openDevTools();
+    global.mwin.on('closed', function() {
+        mainWindow = null;
+    });
 }
 function load()
 {
-  app.on('ready', createWindow);
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', function () {
-  if (global.mwin === null) {
-    createWindow();
-  }
-});
+    app.on('ready', createWindow);
+    app.on('window-all-closed', function() {
+        if (process.platform !== 'darwin') {
+            app.quit();
+        }
+    });
+    app.on('activate', function() {
+        if (global.mwin === null) {
+            createWindow();
+        }
+    });
 }
 module.exports = {
-  load
+    load
 };
